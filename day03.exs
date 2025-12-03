21a923c8e4c2468f059382bbeb75f754cb1a1d93
Jochum Börger <git.eggshell573@passmail.net>
2025-12-03 12:18:32 +0100

    Day 3 cleanup

--- day03.exs
+++ day03.exs
@@ -4,8 +4,11 @@ defmodule Day03 do
   def max_joltage(banks, n\\2, sum\\0)
   def max_joltage([], _, sum), do: sum
   def max_joltage([bank|tail], n, sum) do
-    batteries = String.graphemes(bank) |> Enum.map(&String.to_integer/1)
-    max_joltage(tail, n, select_n_batteries(batteries, n) + sum)
+    bank
+      |> String.graphemes
+      |> Enum.map(&String.to_integer/1)
+      |> select_n_batteries(n)
+      |> (&(max_joltage(tail, n, &1 + sum))).()
   end
 
   defp select_n_batteries(batteries, n, acc\\[])
@@ -13,15 +16,16 @@ defmodule Day03 do
     acc |> Enum.map(&Integer.to_string/1) |> List.to_string |> String.to_integer
   end
   defp select_n_batteries(bs, n, acc) do
-    max = Enum.max(Enum.take(bs, Enum.count(bs) + 1 - n))
-    remaining = Enum.drop_while(bs, &(&1 < max)) |> Enum.drop(1)
-    select_n_batteries(remaining, n-1, acc ++ [max])
+    selected = bs |> Enum.take(Enum.count(bs) + 1 - n) |> Enum.max
+    bs
+      |> Enum.drop_while(&(&1 < selected))
+      |> Enum.drop(1)
+      |> select_n_batteries(n-1, acc ++ [selected])
   end
 
 end
 
-banks = File.stream!("input/day03")
-  |> Enum.map(&String.trim_trailing/1)
+banks = File.stream!("input/day03") |> Enum.map(&String.trim_trailing/1)
 
 banks |> Day03.max_joltage |> IO.inspect(label: "Part 1")
 banks |> Day03.max_joltage(12) |> IO.inspect(label: "Part 2")
